a1fdfbc5508170c0d099970a97613951f5209222
rodzt <11564555+rodztt@users.noreply.github.com>
2025-01-07 09:50:49 -0300

    Fix issue with string values on the response

--- calculateCrossRate.js
+++ calculateCrossRate.js
@@ -1,3 +1,8 @@
+function toFixedNumber(num, digits, base){
+  const pow = Math.pow(base ?? 10, digits);
+  return Math.round(num*pow) / pow;
+}
+
 const  calculateCrossRate= function calculateCrossRate(baseCurrencyReference, targetCurrencyObject) {
     if (baseCurrencyReference.baseCurrency !== targetCurrencyObject.baseCurrency) {
       throw new Error("Base currencies do not match for cross-rate calculation.");
@@ -8,7 +13,7 @@ const  calculateCrossRate= function calculateCrossRate(baseCurrencyReference, ta
       currency: targetCurrencyObject.currency,
       observations: baseCurrencyReference.observations.map((refObservation, index) => ({
         date: refObservation.date,
-        rate: (targetCurrencyObject.observations[index].rate / refObservation.rate).toFixed(4) 
+        rate: Math.round( (targetCurrencyObject.observations[index].rate / refObservation.rate) * 1e4 ) / 1e4 //rounds the number 4 decimals. To fixed was outputing string
       }))
     };
   }
